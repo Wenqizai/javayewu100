8fd1b06d6fd9429fed7360b96f39d69934606227
liangwq <liangwq@gzsendi.cn>
2020-12-22 10:45:29 +0800

    连接池

---
+++ src/main/java/com/wenqi/learning/javayewu100/connectionpool/JedisApplication.java
@@ -0,0 +1,13 @@
+package com.wenqi.learning.javayewu100.connectionpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JedisApplication.class, args);
+    }
+
+}
