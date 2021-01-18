000052c6210d4c2817b0875de8721f89fac8b705
liangwq <liangwq@gzsendi.cn>
2021-01-18 15:01:14 +0800

    equals&==

---
+++ src/main/java/com/wenqi/learning/javayewu100/equals/instandstringequal/OrderQuery.java
@@ -0,0 +1,15 @@
+package com.wenqi.learning.javayewu100.equals.instandstringequal;
+
+import lombok.Data;
+
+/**
+ * @author liangwq
+ * @date 2021/1/18
+ */
+@Data
+public class OrderQuery {
+
+    private Integer status;
+    private String name;
+
+}
---
+++ src/main/java/com/wenqi/learning/javayewu100/equals/lombokequals/CommonMistakesApplication.java
@@ -0,0 +1,15 @@
+package com.wenqi.learning.javayewu100.equals.lombokequals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liangwq
+ * @date 2021/1/18
+ */
+@SpringBootApplication
+public class CommonMistakesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonMistakesApplication.class, args);
+    }
+}
