67156405d8219ed248256c9d5c7aa8e41b276b04
liangwq <liangwq@gzsendi.cn>
2021-02-03 14:38:21 +0800

    集合的坑

---
+++ src/main/java/com/wenqi/learning/javayewu100/numeralcalculations/Equals.java
@@ -0,0 +1,42 @@
+package com.wenqi.learning.javayewu100.numeralcalculations;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author liangwq
+ * @date 2021/1/20
+ */
+public class Equals {
+
+    public static void main(String[] args) {
+        wrong();
+        right();
+        set();
+    }
+
+    private static void wrong() {
+        System.out.println(new BigDecimal("1.0").equals(new BigDecimal("1")));
+    }
+
+    private static void right() {
+        System.out.println(new BigDecimal("1.0").compareTo(new BigDecimal("1")) == 0);
+    }
+
+    private static void set() {
+        Set<BigDecimal> hashSet1 = new HashSet<>();
+        hashSet1.add(new BigDecimal("1.0"));
+        System.out.println(hashSet1.contains(new BigDecimal("1")));//返回false
+
+        Set<BigDecimal> hashSet2 = new HashSet<>();
+        hashSet2.add(new BigDecimal("1.0").stripTrailingZeros());
+        System.out.println(hashSet2.contains(new BigDecimal("1.000").stripTrailingZeros()));//返回true
+
+        Set<BigDecimal> treeSet = new TreeSet<>();
+        treeSet.add(new BigDecimal("1.0"));
+        System.out.println(treeSet.contains(new BigDecimal("1")));//返回true
+    }
+
+}
